b7ee50894398394d7b0806fcbda74c6077a4d800
wwjdtm <jje0ng@kookmin.ac.kr>
2020-09-27 21:01:05 +0900

    스크롤애니메이션 영역구현

--- js/main.js
+++ js/main.js
@@ -10,7 +10,14 @@
             heightNum:5, //브라우저높이의5배로 scrollheight세팅
             scrollHeight: 0,
             objs:{
-                container: document.querySelector('#scroll-section-0')
+                container: document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+                messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+                messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+                messageD: document.querySelector('#scroll-section-0 .main-message.d')
+            },
+            values:{
+                messageA_opacity: [0,1]
             }
 
         
@@ -57,6 +64,17 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
+        yOffset = window.pageYOffset;
+        //currentscene 자동으로로딩
+        let totalScrollHeight=0;
+        for(let i=0; i<sceneInfo.length; i++){
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if(totalScrollHeight>=yOffset){
+                currentScene=i;
+                break;
+            }
+        }
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
     }
     let yOffset=0; //window.pageyoffset대신 쓸 변수
     let prevScrollHeight=0; //모든 섹션의 스크롤합 16420
@@ -70,16 +88,38 @@
         }
         if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
             currentScene++;
+            document.body.setAttribute('id',`show-scene-${currentScene}`);
         }
         if(yOffset < prevScrollHeight){
             if(currentScene===0) return;
             currentScene --;
+            document.body.setAttribute('id',`show-scene-${currentScene}`);
         }
-        console.log(currentScene);
+        // console.log(currentScene);
+
+        playAnimation();
+       
         
     }
+    function playAnimation(){
+        switch (currentScene){
+            case 0:
+                console.log('0 play');
+                break;
+            case 1:
+                console.log('1 play');
+                break;
+            case 2:
+                console.log('2 play');
+                break;
+            case 3:
+                console.log('3 play');
+                break;
+        }
 
-    window.addEventListener('resize',setLayout);
+    }
+
+    
     //스크롤위치ㅣ
     window.addEventListener('scroll', ()=>{
         //스크롤하면 기본적으로 실행되는함수
@@ -88,7 +128,7 @@
         scrollLoop();
 
     });
-
-    setLayout();
+    window.addEventListener('load',setLayout);
+    window.addEventListener('resize',setLayout);
 
 })();//감싸고,함수바로호출됨
